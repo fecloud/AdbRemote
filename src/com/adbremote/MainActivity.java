29eda31d6f0534c1e8af6dfca58899885d53fefa
oyf_feng <oyf_feng@163.com>
2015-12-25 09:18:47 +0800

    add show ip address

--- src/com/adbremote/MainActivity.java
+++ src/com/adbremote/MainActivity.java
@@ -10,6 +10,9 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -24,7 +27,7 @@ import android.widget.TextView;
  */
 public class MainActivity extends Activity implements OnClickListener {
 
-	private TextView textView;
+	private TextView textView, ip;
 
 	private AdbRemoteTask adbRemoteTask;
 
@@ -40,6 +43,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
 		findViewById(R.id.button).setOnClickListener(this);
 		textView = (TextView) findViewById(R.id.textView);
+		ip = (TextView) findViewById(R.id.ip);
 	}
 
 	/*
@@ -53,6 +57,40 @@ public class MainActivity extends Activity implements OnClickListener {
 			adbRemoteTask = new AdbRemoteTask();
 			adbRemoteTask.execute();
 		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
+	protected void onResume() {
+		getActiveNetwork();
+		super.onResume();
+	}
+
+	private void getActiveNetwork() {
+		// 获取wifi服务
+		WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		// 判断wifi是否开启
+		if (wifiManager.isWifiEnabled()) {
+			WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+			if (wifiInfo != null) {
+				int ipAddress = wifiInfo.getIpAddress();
+				String ip = intToIp(ipAddress);
+				this.ip.setText("ip address "+ip);
+			}
+		} else {
+			this.ip.setText(R.string.openwifi);
+		}
+
+	}
+
+	private String intToIp(int i) {
+
+		return (i & 0xFF) + "." + ((i >> 8) & 0xFF) + "." + ((i >> 16) & 0xFF)
+				+ "." + (i >> 24 & 0xFF);
 	}
 
 	private class AdbRemoteTask extends AsyncTask<String, String, Boolean> {
